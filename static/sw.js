e84577e439dbd5ba76ac1bd2250930e91d6f7348
MD SALAHUDDIN <md.salahuddin0985@gmail.com>
2026-08-11 14:39:42 +0600

    Commit changes

---
+++ static/sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'onecity-v1';
+const urlsToCache = [
+  '/',
+  '/static/manifest.json'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        return response || fetch(event.request);
+      })
+  );
+});
